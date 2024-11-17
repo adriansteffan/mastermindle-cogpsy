ba6bf0c348c699abfeb7037dc099da4101c22b0f
Adrian Steffan <adrian.steffan@hotmail.de>
2024-11-17 03:37:13 +0100

    check deployment again

--- backend/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Set ownership and permissions for the data directory
-chown -R node:node /srv/backend/data
-chmod -R 777 /srv/backend/data
-
-# Execute the main command (PM2)
-npx pm2-runtime ./dist/server.js
